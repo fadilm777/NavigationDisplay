7e86a273dd17684b6f8a70fdc7ddaa7a1beb00ed
fadilm777 <fadil.m777@gmail.com>
2025-05-12 14:46:47 -0400

    waypoints addition

--- frontend/main.js
+++ frontend/main.js
@@ -4,10 +4,27 @@ import { middleware } from './src/nav.js';
 
 map.on('load', () => {
 
+  map.addLayer({
+    "id": "Waypoints",
+    "type": "symbol",
+    "source": "waypoints",
+    "layout": {
+      "visibility": "visible",
+      "icon-image": "marker",
+      "icon-size": 1,
+      "text-field": ["get", "name"],
+      "text-anchor": "top",
+      "text-offset": [0, 1.2]
+    },
+    "paint": {
+      "text-color": "#FFFFFF"
+    }
+  })
+
   map.jumpTo({
     center: [-73.742157, 45.464088]
   })
-  const socket = new WebSocket("ws://127.0.0.1:8001")
+  var socket = new WebSocket("ws://127.0.0.1:8001")
 
   socket.addEventListener("message", (event) => {
 
